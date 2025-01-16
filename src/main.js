9de7020f4e704461711caa2def3371c15ef10860
Anastasiia Skliarovska <skliarovskaa@gmail.com>
2025-01-16 14:56:57 +0200

    update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { galleryEl, renderImageCards } from '/js/render-functions.js';
 import iziToast from 'izitoast';
 import SimpleLightbox from 'simplelightbox';
 import './js/scroll-to-up';
-import error from './img/error.svg';
+import errorIcon from '/src/img/error.svg';
 
 const loaderEl = document.querySelector('.js-loader');
 
@@ -33,7 +33,7 @@ const onFormSubmit = event => {
         loaderEl.classList.add('hidden');
 
         iziToast.show({
-          iconUrl: error,
+          iconUrl: errorIcon,
           message:
             'Sorry, there are no images matching your search query. Please try again!',
           position: 'topRight',
